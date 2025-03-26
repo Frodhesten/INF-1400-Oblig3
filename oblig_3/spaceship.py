a22ee9591ffb5caa6dcdc110043d9e4c65a3715c
Frodhesten <frodegg@gmail.com>
2025-03-26 17:10:50 +0100

    testing

--- oblig_3/spaceship.py
+++ oblig_3/spaceship.py
@@ -27,8 +27,6 @@ class Spaceship(pygame.sprite.Sprite):
             self.angle += 5
         if key[pygame.K_RIGHT]:
             self.angle -= 5
-        if key[pygame.K_UP]:
-            self.thrust()
             
         self.image = pygame.transform.rotate(self.original_image, self.angle)
         self.rect = self.image.get_rect(center=self.rect.center)
